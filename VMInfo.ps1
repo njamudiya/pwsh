53af0b2be05dd2c10cc5af2d88bda202231dc7cb
Nitish Jamudiya <n.jamudiya@gmail.com>
2022-09-10 15:55:54 +0530

    Update VMInfo.ps1

--- VMInfo.ps1
+++ VMInfo.ps1
@@ -1,4 +1,21 @@
-Import-Module VMware.PowerCLI
-Connect-VIServer 192.168.1.20 -User 'srini' -Password 'rM)xBj7#'
-Get-VM
-Disconnect-VIServer -Confirm:$false
+Param([String]$pass)
+
+try{
+    Connect-VIServer 192.168.1.20 -User 'srini' -Password 'rM)xBj7#'
+    Get-VM
+    Disconnect-VIServer -Confirm:$false
+}
+catch{
+
+}
+
+$msg = "Test Email"
+$User = "smtp@jamudiya.live"
+$cred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, ($pass| ConvertTo-SecureString)
+try{
+    Send-MailMessage -SmtpServer smtp.zoho.in -Port 587 -From smtp@jamudiya.live -To nitish@jamudiya.live -Body $msg -Subject "VMInfo $((Get-Date).ToString())" -Credential $cred -UseSsl -Verbose
+}
+catch{
+    Write-Host "Failed to send email"
+    Write-Host "Script output - `n$msg"
+}
