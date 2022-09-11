fef0083709f6f24b15abea6a3d041150924e846f
Nitish Jamudiya <n.jamudiya@gmail.com>
2022-09-11 07:31:08 +0530

    Create ESXI_Alert.ps1

---
+++ ESXI_Alert.ps1
@@ -0,0 +1,48 @@
+Param(
+    [String]$smtpuser,
+    [String]$smtppass,
+    [String]$vcuser,
+    [String]$vcpass,
+    $VIServer,
+    [String]$smtpserver,
+    $smtpport,
+    [String]$from,
+    [String]$to
+)
+
+Set-PowerCLIConfiguration  -InvalidCertificateAction Ignore -Confirm:$false
+
+try{
+    Connect-VIServer $VIServer -User $vcuser -Password $vcpass
+    $nuc = Get-VmHost
+    $percpu = ($nuc.CpuUsageMhz)*100/($nuc.CpuTotalMhz)
+    $permem = ($nuc.MemoryUsageGB)*100/($nuc.MemoryTotalGB)
+    if($percpu -ge 50){
+        $sub = "Alert : High CPU utilzation"
+    }
+
+    if($percpu -le 80){
+        $sub = "Alert : High Memory utilzation"
+    }
+
+    $ds = Get-Datastore
+    $ds | foreach{
+        $perstorage = ($_.FreeSpaceGB)*100/($_.CapacityGB)
+        if($perstorage -ge 85){
+            $sub = "Alert : High Staorage utilzation"
+        }
+    }
+
+    $ds | Select Name, FreeSpaceGB, CapacityGB | Format-Table
+    $msg = $nuc | ConvertTo-Html
+    $msg += $ds | ConvertTo-Html
+    Disconnect-VIServer -Confirm:$false
+}
+catch{
+    $msg = $_
+    $sub = "Alert : Failed ot get ESXI information"
+}
+
+if($sub){
+    Send-MailMessage -SmtpServer $smtpserver -Port $smtpport -From $from -To $to -Body "$msg" -Subject "$sub $((Get-Date).ToString())" -Credential $credential -UseSsl -Verbose -BodyAsHtml
+}
